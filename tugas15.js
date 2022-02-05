2de1dccf0cb19c705e9eb31df8afde6e181de0a5
Firman Ismail <akayasha99@gmail.com>
2022-02-05 23:32:33 +0700

    tugas15.js

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+function sisimiring(){
+  var ab = 8;
+  var bc = 6;
+  console.log("Nilai AB : ",ab)
+  console.log("Nilai BC : ",bc);
+  console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah :",Math.sqrt(Math.pow(ab,2)+Math.pow(bc,2)));
+}
+sisimiring()
